ed271f69985676119f14d4762d0dcae724eb0834
Wellington Bernardes <webean@outlook.com>
2020-03-27 14:38:02 -0300

    con

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -10,6 +10,8 @@ module.exports = {
 
   async create(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = generateUniqueId();
 
     const id = crypto.randomBytes(4).toString('HEX');
   
